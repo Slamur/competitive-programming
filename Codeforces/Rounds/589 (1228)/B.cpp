f91b04d6fb7776ca3a13a433a7ff66681a0da703
Слава Муравьёв <MuravjevSlava@gmail.com>
2020-04-18 22:06:39 +0400

    cf 587B

---
+++ Codeforces/Rounds/589 (1228)/B.cpp
@@ -0,0 +1,333 @@
+#define _STRESS
+#ifdef STRESS
+#include "Base.h"
+#else
+#pragma comment(linker, "/STACK:66777216")
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <bitset>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <iomanip>
+#include <functional>
+#include <random>
+
+using namespace std;
+
+typedef uniform_int_distribution<> uid;
+typedef mt19937 rnd_t;
+
+typedef long long ll;
+
+// need define ONLINE_JUDGE before submit
+#ifdef ONLINE_JUDGE
+typedef __int128_t sll;
+#else
+typedef ll sll;
+#endif
+
+typedef pair<int, int> ii;
+
+typedef vector<int> vi;
+typedef vector<size_t> vsz;
+
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+typedef vector<ii> vii;
+typedef vector<vii> vvii;
+
+typedef vector<vi> vvi;
+typedef vvi graph_t;
+
+const char SPACE = ' ', ENDL = '\n';
+
+const int BASE = 1000 * 1000 * 1000 + 7;
+const int MODULO = BASE; // 1000 * 1000 * 1000 + 31; //998244353
+#endif
+
+template <class T>
+T read() {
+	T value;
+	cin >> value;
+	return value;
+}
+
+int read_int() {
+	return read<int>();
+}
+
+size_t read_size() {
+	return read<size_t>();
+}
+
+ll read_ll() {
+	return read<ll>();
+}
+
+ii read_ii() {
+	int x = read_int();
+	int y = read_int();
+	return ii(x, y);
+}
+
+string read_string() {
+	return read<string>();
+}
+
+template <class T>
+void read_vector(vector<T> & a, size_t size) {
+	a.resize(size);
+	for (T & value : a) cin >> value;
+}
+
+template <class T1, class T2>
+void read_pairs(vector<pair<T1, T2>> & a, size_t size) {
+	a.resize(size);
+	for (auto & value : a) cin >> value.first >> value.second;
+}
+
+void add_to_each(vi & a, int delta) {
+	for (int & value : a) value += delta;
+}
+
+void add_to_each(vii & a, int delta) {
+	for (ii & value : a) {
+		value.first += delta;
+		value.second += delta;
+	}
+}
+
+template <class T>
+void print(T & a, char sep = ENDL) {
+	cout << a << sep;
+}
+
+template <class T>
+void print_vector(vector<T> & a, char sep = SPACE) {
+	for (T & value : a) cout << value << sep;
+	if (sep != ENDL) cout << ENDL;
+}
+
+template <class T>
+void print_matrix(vector<vector<T>> & a, char sep = SPACE) {
+	for (vector<T> & row : a) {
+		print_vector(row, sep);
+	}
+}
+
+void print_pairs(vii & a, char sep = ENDL) {
+	for (ii & value : a) cout << value.first << " " << value.second << sep;
+	if (sep != ENDL) cout << ENDL;
+}
+
+int get_bit(ll mask, int bit) {
+	return (int)((mask >> bit) & 1);
+}
+
+ll set_bit(ll mask, int bit) {
+	return mask | (1LL << bit);
+}
+
+ll clear_bit(ll mask, int bit) {
+	return mask & (~(1LL << bit));
+}
+
+vi reverse(vi & a) {
+	reverse(a.begin(), a.end());
+	return a;
+}
+
+typedef ll mod_t;
+
+void update_max(int & cur_value, int new_value) {
+	cur_value = max(cur_value, new_value);
+}
+
+void update_min(int & cur_value, int new_value) {
+	cur_value = min(cur_value, new_value);
+}
+
+ll gcd(ll a, ll b) {
+	return (a == 0 ? b : gcd(b % a, a));
+}
+
+ll lcm(ll a, ll b) {
+	return (a * b == 0 ? 0 : a / gcd(a, b) * b);
+}
+
+bool check_index(int index, int size) {
+	return 0 <= index && index < size;
+}
+
+bool check_cell(int x, int xSize, int y, int ySize) {
+	return check_index(x, xSize) && check_index(y, ySize);
+}
+
+// =============================================
+
+#define RUN
+
+void init() {
+
+}
+
+#ifndef STRESS
+struct Input {
+	int n, m;
+	vi a, b;
+
+	Input(int n, int m, vi a, vi b) : n(n), m(m), a(a), b(b) { }
+};
+#endif
+
+Input read_input() {
+	int n = read_size();
+	int m = read_size();
+
+	vi a;
+	read_vector(a, n);
+
+	vi b;
+	read_vector(b, m);
+
+	return Input(n, m, a, b);
+}
+
+/*
+modular
+binpow
+*/
+
+// =============================================
+
+typedef ll ans_t;
+
+// =============================================
+
+inline mod_t multiply(mod_t a, mod_t b) {
+	return ((a * b) % MODULO);
+}
+
+mod_t binpow(int base, ll power) {
+	if (power == 0) return 1;
+
+	ll result = binpow(base, power >> 1);
+	result = multiply(result, result);
+
+	if (power & 1) {
+		result = multiply(result, base);
+	}
+
+	return result;
+};
+
+ans_t get_answer(Input & input) {
+	int n = input.n, m = input.m;
+	vi & a = input.a;
+	vi & b = input.b;
+
+	vvi field(n, vi(m, 0));
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < a[i]; ++j) {
+			field[i][j] = 1;
+		}
+	}
+
+	for (int j = 0; j < m; ++j) {
+		for (int i = 0; i < b[j]; ++i) {
+			field[i][j] = 1;
+		}
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < a[i]; ++j) {
+			if (!field[i][j]) return 0;
+		}
+
+		if (a[i] < m) {
+			if (field[i][a[i]]) return 0;
+		}
+	}
+
+	for (int j = 0; j < m; ++j) {
+		for (int i = 0; i < b[j]; ++i) {
+			if (!field[i][j]) return 0;
+		}
+
+		if (b[j] < n && field[b[j]][j]) return 0;
+	}
+
+	for (int i = 0; i < n; ++i) {
+		if (a[i] < m) {
+			field[i][a[i]] = 2;
+		}
+	}
+
+	for (int j = 0; j < m; ++j) {
+		if (b[j] < n) {
+			field[b[j]][j] = 2;
+		}
+	}
+
+	int cnt = 0;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			if (!field[i][j]) ++cnt;
+		}
+	}
+
+	ll answer = binpow(2, cnt);
+	return answer;
+}
+
+void print_answer(ans_t answer) {
+	print(answer);
+}
+
+void solve_test() {
+	Input input = read_input();
+
+	ans_t answer = get_answer(input);
+
+	print_answer(answer);
+}
+
+int main() {
+#ifdef RUN
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+	init();
+
+	int tests =
+#ifdef ONLINE_JUDGE
+		1;// read_size();
+#else
+		read_size();
+#endif
+	for (int test = 1; test <= tests; ++test) {
+		solve_test();
+	}
+#else
+	set_print_file("tests.txt");
+	stress();
+	close_print_file();
+#endif
+	return 0;
+}
