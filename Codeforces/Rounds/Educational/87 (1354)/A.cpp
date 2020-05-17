7eaea7796db41a10be01ed0d2085a1699e033aab
Слава Муравьёв <MuravjevSlava@gmail.com>
2020-05-18 02:02:01 +0400

    CF Educ 87 ABC1

---
+++ Codeforces/Rounds/Educational/87 (1354)/A.cpp
@@ -0,0 +1,199 @@
+#pragma comment(linker, "/STACK:66777216")
+
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <bitset>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <iomanip>
+#include <functional>
+#include <random>
+#include <fstream>
+#include <ctype.h>
+
+using namespace std;
+
+typedef uniform_int_distribution<> uid;
+typedef mt19937 rnd_t;
+
+typedef long long ll;
+
+// need define ONLINE_JUDGE before submit
+//#ifdef ONLINE_JUDGE
+//typedef __int128_t sll;
+//#else
+//typedef ll sll;
+//#endif
+
+typedef pair<int, int> ii;
+
+typedef vector<int> vi;
+typedef vector<size_t> vsz;
+
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+typedef vector<ii> vii;
+typedef vector<vii> vvii;
+
+typedef vector<vi> vvi;
+typedef vvi graph_t;
+
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+const char SPACE = ' ', ENDL = '\n';
+
+const int BASE = (int)1e9 + 7;
+const int MODULO = BASE; // 1e9 + 31; //998244353
+
+typedef ll mod_t;
+
+///////////////////////////////////////////////////////////////////
+
+int ri() {
+	int v;
+	cin >> v;
+	return v;
+}
+
+ll rll() {
+	ll v;
+	cin >> v;
+	return v;
+}
+
+string rs() {
+	string s;
+	cin >> s;
+	return s;
+}
+
+vi rvi(int n) {
+	vi a(n);
+	for (int i = 0; i < n; ++i) cin >> a[i];
+	return a;
+}
+
+void read_graph(int n, int edges, graph_t& graph, bool oriented = false) {
+	graph.assign(n, vi());
+	for (int e = 0; e < edges; ++e) {
+		int from = ri() - 1;
+		int to = ri() - 1;
+		graph[from].push_back(to);
+		if (!oriented) graph[to].push_back(from);
+	}
+}
+
+bool yn(bool result, string yes = "YES", string no = "NO") {
+	cout << (result ? yes : no) << ENDL;
+	return result;
+}
+
+//////////////////////////////////////////////////////////////////
+
+int get_bit(int mask, int bit) {
+	return (mask >> bit) & 1;
+}
+
+//////////////////////////////////////////////////////////////////
+
+// taken from https://codeforces.com/blog/entry/76149?#comment-606512
+
+//template<class... args>
+//auto create(size_t n, args&&... args) {
+//	if (constexpr(sizeof...(args) == 1)) {
+//		return vector(n, args...);
+//	}
+//	else
+//	{
+//		return vector(n, create(args...));
+//	}	
+//}
+
+//////////////////////////////////////////////////////////////////
+
+int init_input() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+	FILE* tmp_in;
+	freopen_s(&tmp_in, "input.txt", "r", stdin);
+
+	FILE* tmp_out;
+	freopen_s(&tmp_out, "output.txt", "w", stdout);
+#endif
+
+	cout << setprecision(10) << fixed;
+
+	int local_tests =
+#ifdef ONLINE_JUDGE
+		1;
+#else
+		ri();
+#endif
+
+	return local_tests;
+}
+
+///////////////////////////////////////////////////////////////////
+
+/*
+tag multitest
+tag math
+tag integer division
+*/
+
+void solve_test() {
+	int tests = ri();
+	for (int test = 1; test <= tests; ++test) {
+		int need = ri();
+		int start = ri();
+		int delay = ri();
+		int before_sleep = ri();
+
+		bool can = need <= start || before_sleep < delay;
+		if (!can) {
+			cout << -1;
+		}
+		else if (need <= start) {
+			cout << start;
+		} 
+		else {
+			need -= start;
+
+			int delay_sleep = (delay - before_sleep);
+			ll full = need / delay_sleep;
+
+			ll time = start + full * delay;
+			if (need % delay_sleep > 0) {
+				time += delay;
+			}
+
+			cout << time;
+		}
+
+		cout << ENDL;
+	}
+}
+
+int main() {
+	int local_tests = init_input();
+
+	for (int local_test = 0; local_test < local_tests; ++local_test) {
+		solve_test();
+	}
+
+	return 0;
+}
---
+++ Codeforces/Rounds/Educational/87 (1354)/B.cpp
@@ -0,0 +1,202 @@
+#pragma comment(linker, "/STACK:66777216")
+
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <bitset>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <iomanip>
+#include <functional>
+#include <random>
+#include <fstream>
+#include <ctype.h>
+
+using namespace std;
+
+typedef uniform_int_distribution<> uid;
+typedef mt19937 rnd_t;
+
+typedef long long ll;
+
+// need define ONLINE_JUDGE before submit
+//#ifdef ONLINE_JUDGE
+//typedef __int128_t sll;
+//#else
+//typedef ll sll;
+//#endif
+
+typedef pair<int, int> ii;
+
+typedef vector<int> vi;
+typedef vector<size_t> vsz;
+
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+typedef vector<ii> vii;
+typedef vector<vii> vvii;
+
+typedef vector<vi> vvi;
+typedef vvi graph_t;
+
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+const char SPACE = ' ', ENDL = '\n';
+
+const int BASE = (int)1e9 + 7;
+const int MODULO = BASE; // 1e9 + 31; //998244353
+
+typedef ll mod_t;
+
+///////////////////////////////////////////////////////////////////
+
+int ri() {
+	int v;
+	cin >> v;
+	return v;
+}
+
+ll rll() {
+	ll v;
+	cin >> v;
+	return v;
+}
+
+string rs() {
+	string s;
+	cin >> s;
+	return s;
+}
+
+vi rvi(int n) {
+	vi a(n);
+	for (int i = 0; i < n; ++i) cin >> a[i];
+	return a;
+}
+
+void read_graph(int n, int edges, graph_t& graph, bool oriented = false) {
+	graph.assign(n, vi());
+	for (int e = 0; e < edges; ++e) {
+		int from = ri() - 1;
+		int to = ri() - 1;
+		graph[from].push_back(to);
+		if (!oriented) graph[to].push_back(from);
+	}
+}
+
+bool yn(bool result, string yes = "YES", string no = "NO") {
+	cout << (result ? yes : no) << ENDL;
+	return result;
+}
+
+//////////////////////////////////////////////////////////////////
+
+int get_bit(int mask, int bit) {
+	return (mask >> bit) & 1;
+}
+
+//////////////////////////////////////////////////////////////////
+
+// taken from https://codeforces.com/blog/entry/76149?#comment-606512
+
+//template<class... args>
+//auto create(size_t n, args&&... args) {
+//	if (constexpr(sizeof...(args) == 1)) {
+//		return vector(n, args...);
+//	}
+//	else
+//	{
+//		return vector(n, create(args...));
+//	}	
+//}
+
+//////////////////////////////////////////////////////////////////
+
+int init_input() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+	FILE* tmp_in;
+	freopen_s(&tmp_in, "input.txt", "r", stdin);
+
+	FILE* tmp_out;
+	freopen_s(&tmp_out, "output.txt", "w", stdout);
+#endif
+
+	cout << setprecision(10) << fixed;
+
+	int local_tests =
+#ifdef ONLINE_JUDGE
+		1;
+#else
+		ri();
+#endif
+
+	return local_tests;
+}
+
+///////////////////////////////////////////////////////////////////
+
+/*
+tag multitest
+tag array
+tag prefix
+tag subarray
+*/
+
+void solve_test() {
+	int tests = ri();
+	for (int test = 1; test <= tests; ++test) {
+		string s = rs();
+		
+		int n = s.size();
+		vi a(n);
+		for (int i = 0; i < n; ++i) a[i] = s[i] - '1';
+
+		int inf = n + 1;
+		int answer = inf;
+
+		vi last(3, -1);
+		for (int i = 0; i < n; ++i) {
+			if (i > 0) {
+				int cur = a[i];
+				int prev = a[i - 1];
+
+				if (cur != prev) {
+					int third = (0 + 1 + 2) - cur - prev;
+					int third_last = last[third];
+
+					if (-1 != third_last) {
+						int length = i - third_last + 1;
+						answer = min(answer, length);
+					}
+				}
+			}
+			last[a[i]] = i;
+		}
+
+		cout << answer % inf << ENDL;
+	}
+}
+
+int main() {
+	int local_tests = init_input();
+
+	for (int local_test = 0; local_test < local_tests; ++local_test) {
+		solve_test();
+	}
+
+	return 0;
+}
