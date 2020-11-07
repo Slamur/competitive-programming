8a0f5e3df25885a53c7c8ad3132d7634e2b87e21
Slava Muravev <MuravjevSlava@gmail.com>
2020-11-07 22:00:01 +0300

    Polygon: separated validator and validator with groups

--- Polygon/_Validator_Groups.cpp
+++ Polygon/_Validator_Groups.cpp
@@ -1,4 +1,4 @@
-#include "testlib.h"
+#include "testlib.h"_Validator
 #include <vector>
 #include <utility>
 #include <cmath>
@@ -33,15 +33,20 @@ int main(int argc, char* argv[])
 {
 	registerValidation(argc, argv);
 
-    const int MAX_N = 16;
-    const int MAX_MK = 1'000'000'000;
-    const int MAX_A = 1'000'000'000;
+    struct data { int maxN, maxC; };
 
-    int n = read_int(1, MAX_N, "n", SPACE_SEP);
-    int m = read_int(1, MAX_MK, "m", SPACE_SEP);
-    int k = read_int(1, MAX_MK, "k");
+    constexpr data groups[] = {
+        { 500, 500 },
+        { 300'000, 300'000 },
+        { 300'000, 1000'000'000 },
+    };
 
-    read_ints(n, 1, MAX_A, "a");
+    auto const& group = groups[map<string, int>{ { "2", 1 }, { "3", 2 } }[validator.group()]];
+
+    int n = read_int(1, group.maxN, "n", SPACE_SEP);
+    int k = read_int(1, n, "k");
+
+    read_ints(n, 0, group.maxC, "c");
 
     inf.readEof();
 
