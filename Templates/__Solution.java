b2e4cb055e4f88457d6a1732afb9a6b297d4ddc3
Slava Muravev <MuravjevSlava@gmail.com>
2020-12-29 03:18:54 +0300

    Templates: Added '__Solution.java'

---
+++ Templates/__Solution.java
@@ -0,0 +1,107 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
+public class __Solution {
+
+    private void solve() {
+
+    }
+
+    //////////////////////////////////////////////////////////////////
+
+    private boolean yesNo(boolean yes) {
+        return yesNo(yes, "YES", "NO");
+    }
+
+    private boolean yesNo(boolean yes, String yesString, String noString) {
+        out.println(yes ? yesString : noString);
+        return yes;
+    }
+
+    //////////////////////////////////////////////////////////////////
+
+    private int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    private String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+
+        return tok.nextToken();
+    }
+
+    private String readLine() {
+        try {
+            return in.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////
+
+    private BufferedReader in;
+    private StringTokenizer tok;
+    private PrintWriter out;
+
+    private void initFileIO(String inputFileName, String outputFileName) throws FileNotFoundException {
+        in = new BufferedReader(new FileReader(inputFileName));
+        out = new PrintWriter(outputFileName);
+    }
+
+    private void initConsoleIO() throws IOException {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    private void initIO() throws IOException {
+        Locale.setDefault(Locale.US);
+
+        String fileName = "";
+
+        if (!fileName.isEmpty()) {
+            initFileIO(fileName + ".in", fileName + ".out");
+        } else {
+            if (new File("input.txt").exists()) {
+                initFileIO("input.txt", "output.txt");
+            } else {
+                initConsoleIO();
+            }
+        }
+
+        tok = new StringTokenizer("");
+    }
+
+    //////////////////////////////////////////////////////////////////
+
+    private void run() {
+        try {
+            long timeStart = System.currentTimeMillis();
+
+            initIO();
+            solve();
+            out.close();
+
+            long timeEnd = System.currentTimeMillis();
+            System.err.println("Time(ms) = " + (timeEnd - timeStart));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new __Solution().run();
+    }
+}
