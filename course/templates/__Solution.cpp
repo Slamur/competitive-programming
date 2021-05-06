15b606d81eeb2f9bc02786efafa1e451ae611d2b
Slava Muravev <MuravjevSlava@gmail.com>
2021-05-06 21:18:52 +0300

    course/templates/__Solution.cpp: - added many usings for different vectors - added readd/add/remult/mult/binpow for modulo calculations - added print/print_all/print_pair - added for_pow/gcd/lcm/get_primes - updated init_io

--- course/templates/__Solution.cpp
+++ course/templates/__Solution.cpp
@@ -6,8 +6,51 @@ using ll = int64_t;
 using ld = long double;
 using ii = pair<int, int>;
 using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vii = vector<ii>;
+using vs = vector<string>;
+using vd = vector<double>;
+using vb = vector<bool>;
 
 const char SPACE = ' ', ENDL = '\n';
+
+//////////////////////////////////////////////////////////////////
+
+const int MODULO = 1e9 + 7;
+
+void readd(ll& target, ll add) {
+	target += add;
+	target %= MODULO;
+}
+
+ll add(ll target, ll add) {
+    ll result = target;
+    readd(result, add);
+    return result;
+}
+
+void remult(ll& target, ll mult) {
+	target *= mult;
+	target %= MODULO;
+}
+
+ll mult(ll target, ll mult) {
+    ll result = target;
+    remult(result, mult);
+    return result;
+}
+
+ll binpow(ll base, ll power) {
+	if (0 == power) return 1;
+
+	ll half = binpow(base, power >> 1);
+	remult(half, half);
+
+	if (power & 1) remult(half, base);
+
+	return half;
+}
 
 //////////////////////////////////////////////////////////////////
 
@@ -41,6 +84,22 @@ vector<T> rv(size_t size) {
 
 //////////////////////////////////////////////////////////////////
 
+template<typename T>
+void print(T x) {
+	cout << x << ENDL;
+}
+
+template<typename T>
+void print_all(const vector<T>& values, char sep = SPACE) {
+	for (auto& value : values) cout << value << sep;
+	if (sep != ENDL) cout << ENDL;
+}
+
+template<typename F, typename S>
+void print_pair(const pair<F, S>& values) {
+	cout << values.first << SPACE << values.second << ENDL;
+}
+
 bool yesNo(bool result, string yes = "YES", string no = "NO") {
 	cout << (result ? yes : no) << ENDL;
 	return result;
@@ -48,14 +107,57 @@ bool yesNo(bool result, string yes = "YES", string no = "NO") {
 
 //////////////////////////////////////////////////////////////////
 
-template<typename T>
-void remin(T& target, T source) {
-    target = min(target, source);
+template<typename T, typename S>
+bool remin(T& target, S source) {
+	bool change = target > source;
+	if (change) target = source;
+	return change;
 }
 
-template<typename T>
-void remax(T& target, T source) {
-    target = max(target, source);
+template<typename T, typename S>
+bool remax(T& target, S source) {
+    bool change = target < source;
+	if (change) target = source;
+	return change;
+}
+
+template<typename N, typename D>
+N ceil_div(N numerator, D denominator) {
+	return (numerator - 1) / denominator + 1;
+}
+
+//////////////////////////////////////////////////////////////////
+
+ll for_pow(ll base, int power) {
+	ll result = 1;
+	for (int i = 0; i < power; ++i) result *= base;
+	return result;
+}
+
+ll gcd(ll a, ll b) {
+	return (0 == a) ? b : gcd(b % a, a);
+}
+
+ll lcm(ll a, ll b) {
+	if (0 == a && 0 == b) return 0;
+	return a / gcd(a, b) * b;
+}
+
+vi get_primes(int max_prime) {
+	vector<bool> is_prime(max_prime + 1, true);
+	is_prime[0] = false, is_prime[1] = false;
+
+	vi primes;
+	for (int i = 2; i <= max_prime; ++i) {
+		if (is_prime[i]) {
+			primes.push_back(i);
+			for (int j = i + i; j <= max_prime; j += i) {
+				is_prime[j] = false;
+			}
+		}
+	}
+
+	return primes;
 }
 
 //////////////////////////////////////////////////////////////////
@@ -90,20 +192,33 @@ ld sqrt_ld(ld value) {
 
 //////////////////////////////////////////////////////////////////
 
-void init_io() {
-    string file_name = "";
-    if (!file_name.empty()) {
-        string input_file_name = file_name + ".in";
-        freopen(input_file_name.c_str(), "rt", stdin);
+int get_bit(ll mask, int bit) {
+	return (mask >> bit) & 1;
+}
 
-        string output_file_name = file_name + ".out";
-        freopen(output_file_name.c_str(), "wt", stdout);
-    } else {
-        #ifndef ONLINE_JUDGE
-        string input_file_name = "../../input.txt"; // reading from file in parent of parent of directory with file
-        freopen(input_file_name.c_str(), "rt", stdin);
-        #endif
-    }
+//////////////////////////////////////////////////////////////////
+
+void init_io() {
+    string fn = "";
+    string in_fn = "", out_fn = "";
+
+    #ifndef ONLINE_JUDGE
+    in_fn = "../../input.txt"; // reading from file in parent of parent of directory with file
+    //out_fn = "../../output.txt";
+    #endif
+
+    if (!fn.empty()) {
+		in_fn = fn + ".in";
+		out_fn = fn + ".out";
+	}
+
+	if (!in_fn.empty()) {
+		freopen(in_fn.c_str(), "rt", stdin);
+	}
+
+	if (!out_fn.empty()) {
+		freopen(out_fn.c_str(), "wt", stdout);
+	}
 
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
@@ -111,12 +226,42 @@ void init_io() {
 	cout << setprecision(10) << fixed;
 }
 
-void solve() {
+using graph_t = vvi;
+using ans_t = int;
+
+void precalc() {
 
 }
 
-int main() {
+void solve_test() {
+
+}
+
+void solve() {
+}
+
+signed main() {
     init_io();
-    solve();
+
+    #ifdef STRESS
+
+    stress();
+
+    #else
+
+    precalc();
+
+	int tests =
+	#ifdef LOCAL
+		ri();
+	#else
+		1;
+	#endif
+
+	while (tests --> 0) {
+		solve();
+	}
+	#endif
+
     return 0;
 }
