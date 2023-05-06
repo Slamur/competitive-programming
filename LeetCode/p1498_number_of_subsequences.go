7f696ff39794f62c07d35de190e172c6085ce648
Muravjev Slava <MuravjevSlava@gmail.com>
2023-05-06 10:50:36 +0300

    Update p1498_number_of_subsequences.go

--- LeetCode/p1498_number_of_subsequences.go
+++ LeetCode/p1498_number_of_subsequences.go
@@ -1,27 +1,35 @@
 func numSubseq(nums []int, target int) int {
-    mod := 1000 * 1000 * 1000 + 7
     n := len(nums)
 
-    pows := make([]int, n + 1)
-    pows[0] = 1
-    for i := 1; i <= n; i++ {
-        pows[i] = (pows[i - 1] * 2) % mod
+    sort.Ints(nums)
+
+    usedPows := make([]bool, n)
+
+    for left, right := 0, n - 1; left < n; left++ {
+        for ; right >= left && nums[right] + nums[left] > target; right-- {}
+
+        // nums[right] + nums[left] <= target
+        if left <= right {
+            free := right - left
+            usedPows[free] = true
+        }
     }
 
     answer := 0
 
-    sort.Ints(nums[:])
-
-    right := n - 1
-    for left := 0; left < n; left++ {
-        for ; right >= left && nums[right] + nums[left] > target; right-- {}
- 
-        if left <= right {
-            free := right - left
-            answer += pows[free]
+    mod := 1000 * 1000 * 1000 + 7
+    pow := 1
+    for _, usedPow := range usedPows {
+        if usedPow {
+            answer += pow
             if answer >= mod {
                 answer -= mod
             }
+        }
+
+        pow += pow
+        if pow >= mod {
+            pow -= mod
         }
     }
 
