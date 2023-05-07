9c4bc0a4a6acb7ab1fce1a372b670f9df4590100
Muravjev Slava <MuravjevSlava@gmail.com>
2023-05-07 06:27:39 +0300

    Update p1964_longest_valid_obstacle_course.go

--- LeetCode/p1964_longest_valid_obstacle_course.go
+++ LeetCode/p1964_longest_valid_obstacle_course.go
@@ -1,34 +1,21 @@
 func longestObstacleCourseAtEachPosition(obstacles []int) []int {
     n := len(obstacles)
 
-    inf := -1
-    for _, value := range obstacles {
-        if inf < value {
-            inf = value
-        }
-    }
-    inf++
-
-    lenToMin := make([]int, n + 1)
-    for i := range lenToMin {
-        lenToMin[i] = inf
-    }
+    lenToMin := make([]int, 1, n + 1)
     lenToMin[0] = -1
 
     answer := make([]int, n)
     for i, value := range obstacles {
-        left, right := -1, n + 1
-        for ; left + 1 < right; {
-            mid := (left + right) / 2
-            if lenToMin[mid] > value {
-                right = mid
-            } else {
-                left = mid
-            }
+        firstLarger := sort.Search(len(lenToMin), func(mid int) bool {
+            return lenToMin[mid] > value
+        })
+
+        if len(lenToMin) == firstLarger {
+            lenToMin = append(lenToMin, -1)
         }
 
-        answer[i] = right
-        lenToMin[right] = value
+        answer[i] = firstLarger
+        lenToMin[firstLarger] = value
     }
 
     return answer
