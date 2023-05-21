d9e851c48370921d13b1844d598da65770225b7e
Muravjev Slava <MuravjevSlava@gmail.com>
2023-05-21 14:35:47 +0300

    Update matrix_iterator.go

--- LeetCode/utils/matrix_iterator.go
+++ LeetCode/utils/matrix_iterator.go
@@ -10,3 +10,18 @@ var LEFT = Step{0, 1}
 var RIGHT = Step{0, -1}
 var DOWN = Step{1, 0}
 var UP = Step{-1, 0}
+
+var steps = [...]Step {
+    LEFT,
+    RIGHT,
+    DOWN,
+    UP,
+}
+
+func checkIndex(index, size int) bool {
+    return 0 <= index && index < size
+}
+
+func checkCell(x, y, n int) bool {
+    return checkIndex(x, n) && checkIndex(y, n)
+}
