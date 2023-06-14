74aee3ee541433385504e003933c11455f9ae491
Muravjev Slava <MuravjevSlava@gmail.com>
2023-06-14 17:14:05 +0300

    Create p0530_minimum_difference_in_bst.go

---
+++ LeetCode/p0530_minimum_difference_in_bst.go
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func getMinimumDifference(root *TreeNode) int {
+    values := make([]int, 0)
+
+    var dfs func(*TreeNode) 
+
+    dfs = func(node *TreeNode) {
+        if node == nil {
+            return
+        }
+
+        values = append(values, node.Val)
+
+        dfs(node.Left)
+        dfs(node.Right)
+    }
+
+    dfs(root)
+
+    sort.Ints(values)
+
+    ans := int(1e9)
+    for i := 1; i < len(values); i++ {
+        if ans > values[i] - values[i - 1] {
+            ans = values[i] - values[i - 1]
+        }
+    }
+
+    return ans
+}
