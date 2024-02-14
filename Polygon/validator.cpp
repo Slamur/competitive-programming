ab60fa7597f9f25892d9b4b49a93fc123749f2fc
Viacheslav Muravev <slamurmunch@gmail.com>
2024-02-15 02:29:43 +0600

    Update validator.cpp

--- Polygon/validator.cpp
+++ Polygon/validator.cpp
@@ -1,29 +1,5 @@
 #include "problem_lib.h"
 
-int read_int(int min_value, int max_value, string const& name, int separator = EOLN_SEP) {
-    int result = inf.readInt(min_value, max_value, name);
-
-    if (EOLN_SEP == separator) inf.readEoln();
-    else inf.readSpace();
-
-    return result;
-}
-
-vi read_ints(int size, int min_value, int max_value, string const& name) {
-    vi result = inf.readInts(size, min_value, max_value, name);
-    inf.readEoln();
-    return result;
-};
-
-template <typename T, typename L, typename R>
-void ensure_limits(const string& name, const T& value, const L& min_value, const R& max_value) {
-    ensuref(
-        min_value <= value && value <= max_value,
-        "Expected %s in [%d, %d], but found %d",
-        name.c_str(), min_value, max_value, value
-    );
-}
-
 int main(int argc, char* argv[])
 {
     registerValidation(argc, argv);
