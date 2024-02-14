9896a6fbe0666626930b9beddf6fdea2bcec55c7
Viacheslav Muravev <slamurmunch@gmail.com>
2024-02-15 02:25:41 +0600

    Update and rename _Validator_Groups.cpp to validator_groups.cpp

--- Polygon/validator_groups.cpp
+++ Polygon/validator_groups.cpp
@@ -47,13 +47,13 @@ int main(int argc, char* argv[])
 	struct group_data { int maxN; }
    
 	vector<group_data> groups = {
-		{3}, // samples
+		{NAX_N}, // samples
 		{100}, // group 1
 		{1e4}, // group 2
 		{MAX_N} // group 3
 	};
 
-	int group_index = (validator.group() != "") ? stoi(validator.group()) : groups.size() - 1;
+	int group_index = (validator.group() != "") ? stoi(validator.group()) : 0;
 	auto const& group = groups[group_index];
 
 	int n = read_int(MIN_N, group.maxN, "n");
