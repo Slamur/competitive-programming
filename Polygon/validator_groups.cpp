a42cc25758002fc15dfb12ac7f0da643d280b549
Slava Muravev <slamurmunch@gmail.com>
2025-02-10 15:09:59 +0600

    Update validator_groups.cpp

--- Polygon/validator_groups.cpp
+++ Polygon/validator_groups.cpp
@@ -4,21 +4,21 @@ int main(int argc, char* argv[])
 {
 	registerValidation(argc, argv);
 	
-	const int MIN_N = 1, MAX_N = 1e6;
+	const int minN = 1, maxN = 1e6;
 
 	struct group_data { int maxN; };
    
 	vector<group_data> groups = {
-		{MAX_N}, // samples
+		{maxN}, // samples
 		{100}, // group 1
 		{1e4}, // group 2
-		{MAX_N} // group 3
+		{minN} // group 3
 	};
 
 	int group_index = (validator.group() != "") ? stoi(validator.group()) : 0;
 	auto const& group = groups[group_index];
 
-	int n = read_int(MIN_N, group.maxN, "n");
+	int n = read_int(minN, group.maxN, "n");
 	inf.readEof();
 
 	return 0;
