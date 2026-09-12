efd4fbe65e433ab6280669c6585df56340ec8d0b
Slava Muravev <slamurmunch@gmail.com>
2026-09-12 14:58:02 +0600

    Update validator_groups.cpp

--- Polygon/validator_groups.cpp
+++ Polygon/validator_groups.cpp
@@ -1,25 +1,62 @@
 #include "problem_lib.h"
 
-int main(int argc, char* argv[])
-{
-	registerValidation(argc, argv);
-	
-	const int minN = 1, maxN = 1e6;
+int main(int argc, char* argv[]) {
+    registerValidation(argc, argv);
 
-	struct group_data { int maxN; };
+    const int minN = 1, maxN = 1e9;
+    
+    struct group_data { 
+        int maxN; 
+        bool should_be_prime; 
+        bool should_be_pow_2; 
+        
+        group_data(int maxN)
+        : group_data(maxN, false, false)
+        { }
+        
+        group_data(int maxN, bool should_be_prime, bool should_be_pow_2)
+        : maxN(maxN)
+        , should_be_prime(should_be_prime)
+        , should_be_pow_2(should_be_pow_2)
+        { }
+        
+        void validate_n(int n) const {
+            if (should_be_prime) {
+                ensuref(
+                    is_prime(n),
+                    "Expected prime n, but found %d",
+                    n
+                );
+            }
+            
+            if (should_be_pow_2) {
+                ensuref(
+                    (n & (n - 1)) == 0,
+                    "Expected n = 2**k, but found %d",
+                    n
+                );
+            }
+        }
+    };
+    
+    const int e1 = 1e1, e3 = 1e3, e6 = 1e6;
    
-	vector<group_data> groups = {
-		{maxN}, // samples
-		{100}, // group 1
-		{1e4}, // group 2
-		{minN} // group 3
+	std::vector<group_data> groups = {
+		group_data(maxN), // samples
+		group_data(e1), // group 1
+		group_data(e3), // group 2
+		group_data(e6), // group 3,
+		group_data(maxN, true, false), // group 4,
+		group_data(maxN, false, true), // group 5
+		group_data(maxN) // group 6
 	};
 
-	int group_index = (validator.group() != "") ? stoi(validator.group()) : 0;
-	auto const& group = groups[group_index];
+	auto const& group = groups[group_index()];
 
-	int n = read_int(minN, group.maxN, "n");
-	inf.readEof();
+	int n = inf.readInt(minN, group.maxN, "n");
+    inf.readEoln();
+    
+    group.validate_n(n);
 
-	return 0;
+    inf.readEof();
 }
